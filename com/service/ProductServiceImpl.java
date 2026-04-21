f2dd24218d2dfa08f0905d1e258e0b77a9960bbe
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-21 13:03:39 +0545

    updated

---
+++ com/service/ProductServiceImpl.java
@@ -0,0 +1,27 @@
+package com.service;
+
+import java.util.List;
+
+import com.model.Product;
+
+public class ProductServiceImpl implements ProductService {
+
+	@Override
+	public void addProduct(Product p) {
+		
+		
+	}
+
+	@Override
+	public void deleteProduct(int index) {
+		
+		
+	}
+
+	@Override
+	public List<Product> getAllProducts() {
+		
+		return null;
+	}
+	
+}
