ba947847a7d121a6f7229e73fc8165f85716f701
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-05-02 16:59:02 +0545

    updated

--- com/view/ProductForm.java
+++ com/view/ProductForm.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
 
 public class ProductForm extends JFrame {
 
@@ -36,12 +38,16 @@ public class ProductForm extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(64, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		lblNewLabel.setBounds(132, 10, 252, 35);
+		JLabel lblNewLabel = new JLabel("Add Product");
+		lblNewLabel.setBackground(new Color(64, 128, 128));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
+		lblNewLabel.setBounds(10, 28, 252, 35);
 		contentPane.add(lblNewLabel);
 
 	}
