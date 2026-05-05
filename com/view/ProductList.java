a997aa1fba98d495bc7ef307b949b625faeb0335
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-05-05 21:20:47 +0545

    added productlist page

--- com/view/ProductList.java
+++ com/view/ProductList.java
@@ -5,6 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
 
 public class ProductList extends JFrame {
 
@@ -31,12 +32,17 @@ public class ProductList extends JFrame {
 	 * Create the frame.
 	 */
 	public ProductList() {
+		setTitle("Product List");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 721, 319);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(22, 20, 663, 262);
+		contentPane.add(scrollPane);
 
 	}
-
 }
